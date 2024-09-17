ce45438436773ecb539b18d83142c00a878540cb
Luis Carlos Ortellado <luiscarlosortecabral@gmail.com>
2024-09-17 15:03:52 -0400

    chore: add google fonts

--- App.tsx
+++ App.tsx
@@ -1,5 +1,18 @@
+import {
+  NunitoSans_400Regular,
+  NunitoSans_700Bold,
+  useFonts,
+} from "@expo-google-fonts/nunito-sans";
+
+import { ActivityIndicator } from "react-native";
 import Home from "@screens/Home";
+import { ThemeProvider } from "styled-components";
+import theme from "./src/theme";
 
 export default function App() {
-  return <Home />;
+  const [fontsLoaded] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold });
+  return;
+  <ThemeProvider theme={theme}>
+    {fontsLoaded ? <Home /> : <ActivityIndicator />}
+  </ThemeProvider>;
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -2,7 +2,6 @@ import styled from 'styled-components/native'
 
 export const Container = styled.View`
 flex: 1;
-background-color: #000;
-justify-content: center;
-align-items: center;
+background-color: ${({theme}) => theme.COLORS.GRAY_100 };
+padding: 24px
 `
