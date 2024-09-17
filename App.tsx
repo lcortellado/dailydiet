d6809fcae061f89113eccff6c24ede74a967ef26
Luis Carlos Ortellado <luiscarlosortecabral@gmail.com>
2024-09-17 15:28:13 -0400

    feat: add loading component

--- App.tsx
+++ App.tsx
@@ -1,18 +1,25 @@
+import { ActivityIndicator, StatusBar } from "react-native";
 import {
   NunitoSans_400Regular,
   NunitoSans_700Bold,
   useFonts,
 } from "@expo-google-fonts/nunito-sans";
 
-import { ActivityIndicator } from "react-native";
 import Home from "@screens/Home";
+import { Loading } from "@components/Loading";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/theme";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ NunitoSans_400Regular, NunitoSans_700Bold });
-  return;
-  <ThemeProvider theme={theme}>
-    {fontsLoaded ? <Home /> : <ActivityIndicator />}
-  </ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {!fontsLoaded ? <Home /> : <Loading />}
+    </ThemeProvider>
+  );
 }
